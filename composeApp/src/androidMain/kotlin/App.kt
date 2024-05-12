3de0791426540d223985997e5f579c8b0f42f8b7
richardmachage <rchacha261@gmail.com>
2024-05-12 22:08:27 +0300

    greet function, now returns a list of strings

--- composeApp/src/androidMain/kotlin/App.kt
+++ composeApp/src/androidMain/kotlin/App.kt
@@ -27,7 +27,10 @@ fun App() {
             }
             AnimatedVisibility(showContent) {
                 val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+                Column(
+                    Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
                     Image(painterResource(Res.drawable.compose_multiplatform), null)
                     Text("Compose: $greeting")
                 }
--- shared/src/commonMain/kotlin/Greeting.kt
+++ shared/src/commonMain/kotlin/Greeting.kt
@@ -3,10 +3,15 @@ import kotlin.random.Random
 class Greeting {
     private val platform = getPlatform()
 
-    fun greet(): String {
+    /*fun greet(): String {
        // return "Hello, ${platform.name}!"
         val firstWord = if (Random.nextBoolean()) "Hi" else "Hello"
 
         return  "$firstWord, guess what this is! > ${platform.name.reversed()} !"
+    }*/
+
+    fun greet():List<String> = buildList{
+        add(if (Random.nextBoolean()) "HI" else "Hello")
+        add("Guess what this is! > ${platform.name.reversed()} !" )
     }
 }
